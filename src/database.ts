ea36a094a83e07b17405d64773aa23b36ae9f543
Crawron <azertyaaron@gmail.com>
2021-07-15 09:51:24 -0500

    google sheet env

--- src/database.ts
+++ src/database.ts
@@ -3,12 +3,15 @@ import { SheetDatabase } from "sheets-database"
 import "dotenv/config"
 
 const db = new SheetDatabase(process.env.SHEETDOCID!)
-const googleCreds = require("../google-auth.json")
+// const googleCreds = require("../google-auth.json")
 let authed = false
 
 export async function getDbConnection() {
 	if (!authed) {
-		await db.useServiceAccount(googleCreds)
+		await db.useServiceAccount({
+			client_email: process.env.CLIENTEMAIL!,
+			private_key: process.env.GOOGLEPRIVATEKEY!,
+		})
 		authed = true
 	}
 	await db.sync()
