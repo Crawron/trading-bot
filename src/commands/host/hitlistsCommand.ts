1dea834c3a653b4ea08d87defb7d74f59e48c115
Crawron <azertyaaron@gmail.com>
2021-07-17 20:07:25 -0500

    fixed /host hitlists formatting

--- src/commands/host/hitlistsCommand.ts
+++ src/commands/host/hitlistsCommand.ts
@@ -10,11 +10,11 @@ export const hitlistsCommand = new Command(
 	{
 		action: async (int) => {
 			const hitlists = [...game.players.values()].map((p) => ({
-				name: `${p.name} - ${p.dead ? "🩸 | " : ""}**${p.vp}** ${
+				name: `${p.name}\n${p.dead ? "🩸 | " : ""}**${p.vp}** ${
 					emoji.prestige
 				} - **${p.tokens}** ${emoji.oblivion} | **${
 					p.remainingTrades
-				}** :shaking_hands:`,
+				}** :left_right_arrow:`,
 				value:
 					p.hitList
 						.map(game.getPlayerNameFromId)
@@ -26,10 +26,7 @@ export const hitlistsCommand = new Command(
 			const embed = new RichEmbed({ fields: hitlists })
 				.title(`Everyone's Info`)
 				.image("https://via.placeholder.com/360x1/2f3136/2f3136")
-				.color(colors.digory)
-				.footer(
-					`Player Name (Wounded :drop_of_blood: |) Base Prestige ${emoji.prestige} - Oblivion ${emoji.oblivion} | Remiaining Trades :left_right_arrow: `
-				).raw
+				.color(colors.digory).raw
 
 			const theresPlayers = playersInChannel(int.channel).length > 0
 			int.reply(undefined, theresPlayers, embed)
